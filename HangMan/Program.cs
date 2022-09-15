8abb848a18ef57b141b0ee531abe8b78c7e45ed1
Marco Jankovic <Marcojankovice@hotmail.com>
2022-09-15 10:44:01 +0200

    new

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -13,57 +13,67 @@ namespace HangMan
 
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\t\t\t\t\t\tWelcome To HangMan!");
 
             Random random = new Random();
 
+            // Creating my list with words //
             List<string> wordBank = new List<string>{
                 "sheep", "Laugh", "scarab", "leopard", "tomato", "apple", "snake", "lizzard", "strawberry", "snickers", "jeans", "coat", "cow", "yard", "sun", "moon", "lightning"
             };
 
-            //string[] wordBank = { "sheep", "Laugh", "scarab", "leopard", "tomato", "apple"};
-
+            //Generate random word from list, since list we use count instead of length. //
             string wordToGuess = wordBank[random.Next(0, wordBank.Count)];
-            string wordToGuessUppercase = wordToGuess.ToLower();
+            // Takes the random word and make all letters to lowercase //
+            string wordToGuessLowerCase = wordToGuess.ToLower();
 
+            // Creates a new string where i take the word and it's length, I then loop it and append _ for each letter //
             StringBuilder display = new StringBuilder(wordToGuess.Length);
             for (int i = 0; i < wordToGuess.Length; i++)
                 display.Append('_');
 
+             // Creating 2 separeted list, one for correct guess and one for incorrect. I can check something in the list with the contain method
             List<char> correctGuesses = new List<char>();
             List<char> incorrectGuesses = new List<char>();
 
+            // Deklaring lives and a bool for the while loop, also a lettersRevealed to display letters once correct.
             int lives = 10;
             bool won = false;
             int lettersRevealed = 0;
 
-            string input;
+            string? input;
             char guess;
 
-            while (!won && lives > 0)
+            while (!won && lives >= -1)
             {
-                Console.WriteLine("Guess a Letter: ");
-
+                Console.ForegroundColor = ConsoleColor.Red;               
+                Console.Write("\nGuess a Letter: ");
+                //takes the char that's sent from the players input and makes it to lowercase, I also make sure that it only takes the first index from the inpu.
+                //incase the player types more than 1 chars. 
                 input = Console.ReadLine().ToLower();
                 guess = input[0];
 
+                // Here we have a condition to check if the guessed letter is correct or incorrect
                 if (correctGuesses.Contains(guess))
-                {
-                    Console.WriteLine("You've already tried '{0}', and it was correct!", guess);
+                {                   
+                    Console.WriteLine("\t '{0}', Has already been found, try another letter!", guess);
                     continue;
                 }
                 else if (incorrectGuesses.Contains(guess))
                 {
-                    Console.WriteLine("You've already tried '{0}', and it was wrong!", guess);
+                    Console.WriteLine("\tYou've already tried '{0}', and it was wrong!", guess);                    
                     continue;
                 }
 
-                if (wordToGuessUppercase.Contains(guess))
+                if (wordToGuessLowerCase.Contains(guess))
                 {
+                    // adds the letter input to the list correctGuesses
                     correctGuesses.Add(guess);
 
                     for (int i = 0; i < wordToGuess.Length; i++)
                     {
-                        if (wordToGuessUppercase[i] == guess)
+                        if (wordToGuessLowerCase[i] == guess)
                         {
                             display[i] = wordToGuess[i];
                             lettersRevealed++;
@@ -75,8 +85,12 @@ namespace HangMan
                 }
                 else
                 {
-                    incorrectGuesses.Add(guess);
-                    Console.WriteLine($"Current live: {lives}", guess);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    // adds the letter input to the list incorrectGuesses
+                    incorrectGuesses.Add(guess);                   
+                    Console.Clear();
+                    Console.WriteLine($"\tCurrent live: {lives}", guess);
+                    // decrement lives if guess is wrong
                     lives--;
                 }
 
